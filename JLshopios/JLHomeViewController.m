d927e053c16a9397aa4d66dbea7b793436b264d6
daxiongdi <crazydazzle4@163.com>
2016-08-07 14:27:54 +0800

    城市选择器

--- JLshopios/JLHomeViewController.m
+++ JLshopios/JLHomeViewController.m
@@ -10,14 +10,18 @@
 #import "JLShopCollectionViewCell.h"
 #import "SDCycleScrollView.h"
 #import "JLTypeListView.h"
-#import "TSLocateView.h"
+
+#import "BAddressPickerController.h"
+
+#import "JLCityController.h"
+
 
 #define JLHomeCell @"JLHomeViewShopingCell"
 //#define kDuration 0.3
 static const double kDuration = 0.3;
 
 
-@interface JLHomeViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,SDCycleScrollViewDelegate,UIActionSheetDelegate>
+@interface JLHomeViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,SDCycleScrollViewDelegate,JLCityControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIView *topView;
 @property (weak, nonatomic) IBOutlet UIButton *cityPickerButton;
 
@@ -36,35 +40,27 @@ static const double kDuration = 0.3;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
     self.topSearchView.layer.cornerRadius = 6;
     self.topSearchView.layer.borderColor = [UIColor colorWithRed:233/255.0 green:233/255.0 blue:233/255.0 alpha:1].CGColor;
     self.topSearchView.layer.borderWidth = 1;
-    
     [self loadCollectionView];
     //判断是否有选择过城市，有就加载之前的，没有就弹出选择器
     [self isSelectedCity];
 }
 
 -(void)loadCollectionView{
-//    self.collectionView = [[UICollectionView alloc]init];
-    
+    //    self.collectionView = [[UICollectionView alloc]init];
     UICollectionViewFlowLayout *layout =[[UICollectionViewFlowLayout alloc] init];
-    
     layout = [[UICollectionViewFlowLayout alloc] init];
     layout.minimumInteritemSpacing = 0;
     layout.minimumLineSpacing = 0;
     [layout setScrollDirection:UICollectionViewScrollDirectionVertical]; // 垂直滚动
     [layout setHeaderReferenceSize:CGSizeMake([UIScreen mainScreen].bounds.size.width, 270)];
     self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
-    
     self.collectionView.backgroundColor = [UIColor colorWithRed:244/255.0 green:244/255.0 blue:247/255.0 alpha:1];
     self.collectionView.dataSource = self;
     self.collectionView.delegate = self;
-    
     self.collectionView.scrollsToTop = YES;
-    
     {
         UIView *pview = self.view;
         UICollectionView *sview = self.collectionView;
@@ -79,7 +75,7 @@ static const double kDuration = 0.3;
     }
     
     [_collectionView registerNib:[UINib nibWithNibName:@"JLShopCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:JLHomeCell];
-
+    
     self.collectionView.mj_header = [MJRefreshGifHeader headerWithRefreshingBlock:^{
         [self.collectionView.mj_header endRefreshing];
     }];
@@ -98,7 +94,6 @@ static const double kDuration = 0.3;
     self.cycleScrollview = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, creenWidth, 140) delegate:self placeholderImage:[UIImage imageNamed:@"placeholder"]];
     
     _cycleScrollview.currentPageDotImage = [UIImage imageNamed:@"FYTXCategories_page_dot_press"];
-//    _cycleScrollview.pageDotImage = [UIImage imageNamed:@"FYTXCategories_page_dot"];
     _cycleScrollview.imageURLStringsGroup = @[@"guide_page_1",@"guide_page_2",@"guide_page_3"];
     _cycleScrollview.pageControlAliment = SDCycleScrollViewPageContolAlimentCenter;
     [self.collectionView addSubview:_cycleScrollview];
@@ -147,21 +142,26 @@ static const double kDuration = 0.3;
 
 - (void)cycleScrollView:(SDCycleScrollView *)cycleScrollView didSelectItemAtIndex:(NSInteger)index
 {
-//    [[self.viewModel openBannerWith:index]execute:nil];
+    //    [[self.viewModel openBannerWith:index]execute:nil];
 }
 
 #pragma mark - <<<<<<<<<<<<<<<  ButtonMethed  >>>>>>>>>>>>>>>
 
 - (IBAction)cityPicker:(UIButton *)sender
 {
-    /**
-     * 新建一个view来装载这个城市选择器
-     */
-    citySelectWindow_ = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height)];
-    citySelectWindow_.backgroundColor = [UIColor clearColor];
-    [self.view insertSubview:citySelectWindow_ aboveSubview:[[UIApplication sharedApplication].delegate window]];
-    TSLocateView *locateView = [[TSLocateView alloc] initWithTitle:@"定位城市" delegate:self];
-    [locateView showInView:citySelectWindow_];
+    JLCityController *ctrl = [[JLCityController alloc]init];
+    [ctrl setDelegate:self];
+    UINavigationController *na = [[UINavigationController alloc]initWithRootViewController:ctrl];
+    [self presentViewController:na animated:YES completion:^{
+        
+    }];
+}
+
+
+#pragma mark ----------------- citydelegate
+
+-(void)chooseCity:(NSString *)city{
+    [_cityPickerButton setTitle:city forState:UIControlStateNormal];
 }
 
 
@@ -188,9 +188,9 @@ static const double kDuration = 0.3;
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-
-        JLShopCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:JLHomeCell forIndexPath:indexPath];
-
+    
+    JLShopCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:JLHomeCell forIndexPath:indexPath];
+    
     
     
     return cell;
@@ -200,9 +200,9 @@ static const double kDuration = 0.3;
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     CGFloat cellWidth = [UIScreen mainScreen].bounds.size.width/2;
-
+    
     CGSize bsize = CGSizeMake(cellWidth, 100);
-
+    
     return bsize;
 }
 
@@ -220,22 +220,6 @@ static const double kDuration = 0.3;
     return 1;
 }
 
-#pragma mark - <<<<<<<<<<<<<<<  ActionSheetDelegate  >>>>>>>>>>>>>>>
--(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
-{
-    TSLocateView *locateView = (TSLocateView *)actionSheet;
-    TSLocation *location = locateView.locate;
-//    NSLog(@"city:%@ lat:%f lon:%f", location.city, location.latitude, location.longitude);
-    //You can uses location to your application.
-    if(buttonIndex == 0) {
-        NSLog(@"Cancel");
-    }else {
-        UIButton *cityButton = _cityPickerButton;
-        [cityButton setTitle:location.city forState:UIControlStateNormal];
-        [[NSUserDefaults standardUserDefaults] setObject:location.city forKey:@"SELECTEDCITY"];
-        NSLog(@"Select");
-    }
-    [citySelectWindow_ performSelector:@selector(removeFromSuperview) withObject:nil afterDelay:kDuration];
-}
+
 
 @end
