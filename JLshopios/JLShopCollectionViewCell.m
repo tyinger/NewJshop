78ec3428173776fe8f67a04a67ed1f488af19320
陈霖 <584180160@qq.com>
2016-12-21 18:27:52 +0800

    no message

--- JLshopios/JLShopCollectionViewCell.m
+++ JLshopios/JLShopCollectionViewCell.m
@@ -62,34 +62,34 @@
 }
 
 -(void)setHotGoodsValue{
-//    if (self.hotGoodsArray.count >= 2) {
-//        {
-//            JLGoodModel *model = self.hotGoodsArray[0];
-//            self.hotGoodsLeftText.text = model.name;
-//            NSArray *previewImage = model.previewImgs;
-//            NSDictionary *dic = [previewImage firstObject];
-//            NSString *imageURL_left = dic[@"path"];
-//            
-//            if (imageURL_left) {
-//                [self.hotGoodsLeftImage sd_setImageWithURL:[NSURL URLWithString:imageURL_left]];
-//            }
-//        }
-//        
-//        {
-//            JLGoodModel *model = self.hotGoodsArray[1];
-//            self.hotGoodsRightText.text = model.name;
-//            NSArray *previewImage = model.previewImgs;
-//            NSDictionary *dic = [previewImage firstObject];
-//
-//            NSString *imageURL_right = dic[@"path"];;
-//            if (imageURL_right) {
-//                [self.hotGoodsRightImage sd_setImageWithURL:[NSURL URLWithString:imageURL_right]];
-//            }
-//        }
-//    }else{
-//       self.hotGoodsRightText.hidden = self.hotGoodsRightImage.hidden = self.hotGoodsLeftImage.hidden = self.hotGoodsLeftText.hidden = YES;
-//        
-//    }
+    if (self.hotGoodsArray.count >= 2) {
+        {
+            JLGoodModel *model = self.hotGoodsArray[0];
+            self.hotGoodsLeftText.text = model.name;
+            NSArray *previewImage = model.previewImgs;
+            NSDictionary *dic = [previewImage firstObject];
+            NSString *imageURL_left = dic[@"path"];
+            
+            if (imageURL_left) {
+                [self.hotGoodsLeftImage sd_setImageWithURL:[NSURL URLWithString:imageURL_left]];
+            }
+        }
+        
+        {
+            JLGoodModel *model = self.hotGoodsArray[1];
+            self.hotGoodsRightText.text = model.name;
+            NSArray *previewImage = model.previewImgs;
+            NSDictionary *dic = [previewImage firstObject];
+
+            NSString *imageURL_right = dic[@"path"];
+            if (imageURL_right) {
+                [self.hotGoodsRightImage sd_setImageWithURL:[NSURL URLWithString:imageURL_right]];
+            }
+        }
+    }else{
+       self.hotGoodsRightText.hidden = self.hotGoodsRightImage.hidden = self.hotGoodsLeftImage.hidden = self.hotGoodsLeftText.hidden = YES;
+        
+    }
 
 }
 
